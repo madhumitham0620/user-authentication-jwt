d1709ce66db9d1fe8f055df8d24921605edfdc34
madhumitham0620 <madhumitham0620@gmail.com>
2026-09-08 19:29:31 +0530

    Fix Gmail SMTP connection

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,14 +14,14 @@ const db = require("../models/userModel");
 // ==========================================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
-
 
 // ==========================================
 // REGISTER
